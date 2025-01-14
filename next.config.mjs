64eacb054eaaa60751957bfaf86240c6ee5e146e
rabreu2 <rayabreu2296@gmail.com>
2025-01-14 16:38:07 -0500

    static page generation timeout issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = {
+    // time in seconds of no pages generating during static
+    // generation before timing out
+    staticPageGenerationTimeout: 1000,
+  }
 
 export default nextConfig;
