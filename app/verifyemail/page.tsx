9af57ce4351c9be6104a4b916efad20deaf527d4
rabreu2 <rayabreu2296@gmail.com>
2025-01-14 15:25:22 -0500

    moved  verifyUserEmail method to inside of the effect | changed var use into const

--- app/verifyemail/page.tsx
+++ app/verifyemail/page.tsx
@@ -9,21 +9,21 @@ export default function VerifyEmailPage() {
     const [token, setToken] = useState("");
     const [verified, setVerified] = useState(false);
     const [error, setError] = useState(false);
-
-    const verifyUserEmail = async () => {
-        try {
-            await axios.post("/api/users/verifyemail", {token});
-            setVerified(true);
-        } catch (error:any) {
-            setError(true);
-            console.log(error.response.data);
-        }
-    };
+    
     useEffect(() => {
         const urlToken = window.location.search.split("=")[1];
         setToken(urlToken || "");
     }, []);
     useEffect(() => {
+        const verifyUserEmail = async () => {
+            try {
+                await axios.post("/api/users/verifyemail", {token});
+                setVerified(true);
+            } catch (error:any) {
+                setError(true);
+                console.log(error.response.data);
+            }
+        };
         if (token.length > 0) {
             verifyUserEmail();
         }
