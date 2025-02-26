a7da2be4a09453b16436e26d15bbc49083abeb69
rabreu2 <rayabreu2296@gmail.com>
2025-02-25 19:26:23 -0500

    added title capitilization rules | added styling to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,6 +8,8 @@ import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { useSearchParams } from "next/navigation";
 import styled from "styled-components";
+import logo from "../../public/grocery4me-icon.png";
+import Image from "next/image";
 
 const Hero = styled.div`
   min-height: 88vh;
@@ -18,82 +20,104 @@ const Hero = styled.div`
   color: #000;
 `
 
+const RecipeName = styled.h1`
+    margin: 2rem 0;
+    font-size: 2.3rem;
+    font-weight: 600;
+    line-height: 1.1;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+`
+
 const LoginPage: React.FC = () => {
-    const router = useRouter();
-    const [loading, setLoading] = React.useState(false);
-    const [user, setUser] = React.useState({
-        email: "",
-        password : "",
-    });
-    const {isLoggedIn, setIsLoggedIn} =  useContext(LoginContext)!;
+  const router = useRouter();
+  const [loading, setLoading] = React.useState(false);
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+  const { isLoggedIn, setIsLoggedIn } = useContext(LoginContext)!;
 
-    const onLogin = async () => {
-        try {
-            if (isLoggedIn) {
-                router.push("/");
-            }
-            setLoading(true);
-            await axios.post("/api/users/login", user);
-            setIsLoggedIn(true);
-            router.push("/");
-        } catch (error:any) {
-            console.log("Login failed", error.message);
-        } finally {
-            setLoading(false);
-        }
+  const onLogin = async () => {
+    try {
+      if (isLoggedIn) {
+        router.push("/");
+      }
+      setLoading(true);
+      await axios.post("/api/users/login", user);
+      setIsLoggedIn(true);
+      router.push("/");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    const searchParams = useSearchParams();
+  const searchParams = useSearchParams();
 
-    useEffect(() => {
-        // Check for the message in the query parameters
+  useEffect(() => {
+    // Check for the message in the query parameters
 
-        const message = searchParams?.get('message');
-    
-        if (message) {
-          toast.info(message as string, {
-            position: 'top-right',
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: 'colored',
-          });
-        }
-      }, [searchParams]);
+    const message = searchParams?.get('message');
 
-    return (
-        <Hero>
-            <ToastContainer />
-            <h1>{loading ? "Processing" : "Login"}</h1>
-            
-            <label htmlFor="email">Email</label>
-            <input 
-                id="email"
-                type="text"
-                value={user.email}
-                onChange={(e) => setUser({...user, email: e.target.value})}
-                placeholder="email"
-                />
-            <label htmlFor="password">Password</label>
-            <input 
-                id="password"
-                type="password"
-                value={user.password}
-                onChange={(e) => setUser({...user, password: e.target.value})}
-                placeholder="password"
-                />
-            <button onClick={onLogin}>Login</button>
-        </Hero>
-    )
+    if (message) {
+      toast.info(message as string, {
+        position: 'top-right',
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored',
+      });
+    }
+  }, [searchParams]);
+
+  return (
+    <Hero>
+      <ToastContainer />
+      <div className="grid">
+        <RecipeName>{loading ? "Processing" : ""}<Image
+          priority={true}
+          src={logo}
+          width={75}
+          height={45}
+          alt="Recipe4Me Logo"
+        /></RecipeName>
+
+        <input
+          id="email"
+          type="text"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          required
+          className="pl-2 pr-4 py-1 pt-4 text-gray-600 text-lg border rounded-xl focus:outline-none focus:border-gray-600 hover:bg-gray-100"
+        />
+        <label className="text-sm transform -translate-y-[50px] -translate-x-[-10px]" htmlFor="email">Email<span className="text-red-500">*</span></label>
+
+        <input
+          id="password"
+          type="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          required
+          className="pl-2 pr-4 py-1 pt-4 text-gray-600 text-lg border rounded-xl focus:outline-none focus:border-gray-600 hover:bg-gray-100"
+        />
+        <label className="text-sm transform -translate-y-[50px] -translate-x-[-10px]" htmlFor="password">Password<span className="text-red-500">*</span></label>
+
+        <button className="py-1 mt-1 text-lg text-gray-800 bg-[#22b14c] border rounded-xl hover:bg-[#187e37]" onClick={onLogin}>Log in</button>
+      </div>
+    </Hero>
+  )
 }
 
 const LoginPageWithSuspense = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-      <LoginPage />
-    </Suspense>
-  );
+  <Suspense fallback={<div>Loading...</div>}>
+    <LoginPage />
+  </Suspense>
+);
 
 export default LoginPageWithSuspense;
--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -40,7 +40,6 @@ const ListBox = styled.div`
     border-radius: 0.5rem;
 `
 
-
 const RecipeName = styled.li`
     margin-left: 15px;
     margin-bottom: 2px;
@@ -54,7 +53,6 @@ const RecipeExtras = styled.li`
     display: flex;
 `
 
-
 const Recipes = () => {
     const router = useRouter();
     const [recipes, setRecipes] = useState<any[]>([]);
@@ -64,7 +62,6 @@ const Recipes = () => {
         const returnedRecipes = localStorage.getItem("recipes");
         if (returnedRecipes) {
             setRecipes(JSON.parse(returnedRecipes).results);
-            console.log(recipes.toString);
             setRecipeNumber(JSON.parse(returnedRecipes).totalResults);
         }
     }, []);
@@ -79,6 +76,7 @@ const Recipes = () => {
             localStorage.setItem("recipes", JSON.stringify(response.data.data));
 
             setRecipes(response.data.data.results);
+            setRecipeNumber(response.data.data.totalResults);
             router.push("/recipes");
         } catch (error: any) {
             console.error(error);
@@ -93,6 +91,16 @@ const Recipes = () => {
         await loadRecipesFromServer(newOffset);
     }
 
+    const capitalizeTitle = (str: string): string => {
+        const prepositions = new Set(["of", "and", "a", "in", "on", "at", "to", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "from", "up", "down", "over", "under", "again", "further", "then", "once"]);
+
+        return str.replace(/\b\w+\b/g, (word, index) => {
+            return index === 0 || !prepositions.has(word.toLowerCase())
+                ? word.charAt(0).toUpperCase() + word.slice(1)
+                : word.toLowerCase();
+        });
+    };
+
     return (
         <Hero>
             {recipes.length === 0 ? (
@@ -101,37 +109,34 @@ const Recipes = () => {
                 <ResultBox>
                     <RecipeForm setRecipes={setRecipes} className="w-full my-[30px]" />
                     {recipes.map((recipe) => (
-                        <ListBox className='hover:bg-[#c9c7b9]' key={recipe.id}>
-                            <Link className='contents' href={{
-                                pathname: '/recipe',
-                                query: { id: recipe.id }
-                            }}>
+                        <Link className='group contents' href={`/recipe/${recipe.id}`} passHref>
+                            <ListBox className='hover:bg-[#c9c7b9]' key={recipe.id}>
                                 <ImageWithFallback src={recipe.image}
                                     alt="Recipe Image"
                                     width={200}
                                     height={200}
                                     fallbackSrc={img}
                                 />
-                            </Link>
-                            <div className="mt-3">
-                                <RecipeName>
-                                    <Link className='hover:underline' href={`/recipe/${recipe.id}`} passHref>{recipe.title}</Link>
-                                </RecipeName>
-                                <RecipeExtras>
-                                    <ClockIcon className="w-4 h-4" />&nbsp;{recipe.readyInMinutes} mins&nbsp;&nbsp;|&nbsp;&nbsp;<UserIcon className="w-4 h-4" />&nbsp;{recipe.servings}
-                                </RecipeExtras>
-                                <div dangerouslySetInnerHTML={{ __html: recipe.summary.replace(/(<([^>]+)>)/ig, "") }} style={{
-                                    overflow: 'hidden',
-                                    marginLeft: '15px',
-                                    textOverflow: 'ellipsis',
-                                    fontSize: '1rem',
-                                    display: '-webkit-box',
-                                    WebkitLineClamp: 3,
-                                    WebkitBoxOrient: 'vertical',
-                                    lineHeight: '1.5em'
-                                }} />
-                            </div>
-                        </ListBox>
+                                <div className="mt-3">
+                                    <RecipeName className="group-hover:underline">
+                                        {capitalizeTitle(recipe.title.replace(/^.*\?\?/, ""))}
+                                    </RecipeName>
+                                    <RecipeExtras>
+                                        <ClockIcon className="w-4 h-4" />&nbsp;{recipe.readyInMinutes} mins&nbsp;&nbsp;|&nbsp;&nbsp;<UserIcon className="w-4 h-4" />&nbsp;{recipe.servings}
+                                    </RecipeExtras>
+                                    <div dangerouslySetInnerHTML={{ __html: recipe.summary.replace(/(<([^>]+)>)/ig, "").replace(/^(.)/, (match: string) => match.toUpperCase()) }} style={{
+                                        overflow: 'hidden',
+                                        marginLeft: '15px',
+                                        textOverflow: 'ellipsis',
+                                        fontSize: '1rem',
+                                        display: '-webkit-box',
+                                        WebkitLineClamp: 3,
+                                        WebkitBoxOrient: 'vertical',
+                                        lineHeight: '1.5em'
+                                    }} />
+                                </div>
+                            </ListBox>
+                        </Link>
                     ))}
                 </ResultBox>
             )}
