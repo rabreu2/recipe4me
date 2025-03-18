846f17cd8ce6affef8a2573c96d2f0f6a3cfaca5
rabreu2 <rayabreu2296@gmail.com>
2025-03-18 17:06:39 -0400

    users can now save recipes | fixed incorrect env var name in getDatafromToken | added savedRecipes array of numbers to userModel | minor formatting changes

--- app/api/users/verifyemail/route.ts
+++ app/api/users/verifyemail/route.ts
@@ -7,11 +7,11 @@ connect();
 export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json();
-        const {token} = reqBody;
-        const user = await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}});
+        const { token } = reqBody;
+        const user = await User.findOne({ verifyToken: token, verifyTokenExpiry: { $gt: Date.now() } });
 
         if (!user) {
-            return NextResponse.json({error: "Invalid token"}, {status: 400});
+            return NextResponse.json({ error: "Invalid token" }, { status: 400 });
         }
 
         user.isVerified = true;
@@ -24,8 +24,7 @@ export async function POST(request: NextRequest) {
             success: true
         });
     } catch (error: any) {
-        return NextResponse.json({error: error.message},
-            {status: 500})
-        
+        return NextResponse.json({ error: error.message },
+            { status: 500 })
     }
 }
--- src/helper/getDataFromToken.ts
+++ src/helper/getDataFromToken.ts
@@ -8,7 +8,7 @@ export const getDataFromToken = (request: NextRequest) => {
         const token = request.cookies.get("token")?.value || '';
 
         // Verify and decode the token using the secret key
-        const decodedToken:any = jwt.verify(token, process.env.TOKEN_SECRET!);
+        const decodedToken:any = jwt.verify(token, process.env.AUTH_SECRET!);
 
         // Return the user ID from the decoded token
         return decodedToken.id;
